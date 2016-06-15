48822b6d507ae5508421c7b9470094ceb0b25cc5
ogl4jo3 <ogl4jo3@gmail.com>
2016-06-15 14:25:21 +0800

    Replace SQLite by LitePal

---
+++ app/src/main/java/hearsilent/kuas/food/models/ShopModel.java
@@ -0,0 +1,98 @@
+package hearsilent.kuas.food.models;
+
+import org.litepal.annotation.Column;
+import org.litepal.crud.DataSupport;
+
+public class ShopModel extends DataSupport {
+
+	@Column(unique = true) private String name;
+	private String address;
+	private double lat;
+	private double lng;
+	private String region;
+	private String type;
+	private String time;
+	private String description;
+	private String image;
+	private String phone;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLng() {
+		return lng;
+	}
+
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
